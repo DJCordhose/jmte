4ae12f96167185e3e1cf4620cc05346b8064d7fe
oliver.zeigermann <oliver.zeigermann@e0cfbd62-f62f-11d8-30af-f3ed57e6e8f3>
2010-05-18 21:52:19 +0000

    Fixed strange bug not allowing reflection on Entry.Map.getKey / getValue

--- trunk/src/com/floreysoft/jmte/guts/Util.java
+++ trunk/src/com/floreysoft/jmte/guts/Util.java
@@ -1,11 +1,16 @@
 package com.floreysoft.jmte.guts;
 
 import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Util {
 	public static String trimFront(String input) {
@@ -15,12 +20,27 @@ public class Util {
 		return input.substring(i);
 	}
 
-	public static Object getPropertyValue(Object o, String attributeName) {
+	@SuppressWarnings("unchecked")
+	public static Object getPropertyValue(Object o, String attributeName)
+			throws IntrospectionException, IllegalArgumentException,
+			IllegalAccessException, InvocationTargetException,
+			SecurityException, NoSuchFieldException {
 		Object result = null;
-		try {
-			BeanInfo beanInfo = Introspector.getBeanInfo(o.getClass());
-			PropertyDescriptor[] propertyDescriptors = beanInfo
-					.getPropertyDescriptors();
+		BeanInfo beanInfo = Introspector.getBeanInfo(o.getClass());
+		PropertyDescriptor[] propertyDescriptors = beanInfo
+				.getPropertyDescriptors();
+		// XXX this is so strange, can not call invoke on key and value for
+		// Map.Entry, so we have to get this done like this:
+		if (o instanceof Map.Entry) {
+			Map.Entry entry = (Entry) o;
+			if (attributeName.equals("key")) {
+				result = entry.getKey();
+			} else if (attributeName.equals("value")) {
+				result = entry.getValue();
+			}
+
+		} else {
+
 			for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
 				if (propertyDescriptor.getName().equals(attributeName)) {
 					Method readMethod = propertyDescriptor.getReadMethod();
@@ -36,8 +56,6 @@ public class Util {
 					result = field.get(o);
 				}
 			}
-
-		} catch (Exception e) {
 		}
 		return result;
 	}
