e7b42539eaf57ae5f007824920ba49306da3d1bd
oliver.zeigermann <oliver.zeigermann@e0cfbd62-f62f-11d8-30af-f3ed57e6e8f3>
2010-07-19 14:59:30 +0000

    Made segments of expression readable to public

--- trunk/src/com/floreysoft/jmte/ExpressionToken.java
+++ trunk/src/com/floreysoft/jmte/ExpressionToken.java
@@ -4,14 +4,29 @@ import java.util.Map;
 
 
 public abstract class ExpressionToken extends AbstractToken {
-	protected final String[] segments;
+	private final String[] segments;
 
 	protected transient Object evaluated = null;
 	
 	public ExpressionToken(String[] segments) {
+		if (segments == null) {
+			throw new IllegalArgumentException("Parameter segements must not be null");
+		}
 		this.segments = segments;
 	}
 
+	public boolean isComposed() {
+		return getSegments().length > 1;
+	}
+	
+	public boolean isEmpty() {
+		return getSegments().length == 0;
+	}
+	
 	public abstract Object evaluate(Map<String, Object> model,
 			ErrorHandler errorHandler);
+
+	public String[] getSegments() {
+		return segments;
+	}
 }
