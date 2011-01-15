7880c2ec9890de518d5880a11f0d5596c2ea8dbd
oliver.zeigermann <oliver.zeigermann@e0cfbd62-f62f-11d8-30af-f3ed57e6e8f3>
2011-01-15 11:12:50 +0000

    Added unique class name generation for compiled templates.

--- src/com/floreysoft/jmte/Compiler.java
+++ src/com/floreysoft/jmte/Compiler.java
@@ -28,6 +28,19 @@ import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.util.TraceClassVisitor;
 
+import com.floreysoft.jmte.util.UniqueNameGenerator;
+
+/**
+ * 
+ * @author olli
+ * 
+ * @see http://asm.ow2.org/
+ * @see http
+ *      ://java.sun.com/docs/books/jvms/second_edition/html/ClassFile.doc.html
+ * @see http 
+ *      ://java.sun.com/docs/books/jvms/second_edition/html/Instructions.doc.
+ *      html
+ */
 public class Compiler {
 
 	@SuppressWarnings("unchecked")
@@ -46,25 +59,41 @@ public class Compiler {
 		}
 	};
 
-	private static MyClassLoader MY_CLASS_LOADER = new MyClassLoader();
+	private final static MyClassLoader MY_CLASS_LOADER = new MyClassLoader();
+
+	private final static String COMPILED_TEMPLATE_NAME_PREFIX = "com/floreysoft/jmte/compiledTemplates/Template";
+
+	private final static UniqueNameGenerator<String, String> uniqueNameGenerator = new UniqueNameGenerator<String, String>(
+			COMPILED_TEMPLATE_NAME_PREFIX, 0);
 
 	protected final String template;
 	protected final Engine engine;
 	protected final Lexer lexer = new Lexer();
 	protected final LinkedList<Token> scopes = new LinkedList<Token>();
 	protected transient ClassVisitor classVisitor;
+	protected transient ClassWriter classWriter;
+	protected final String superClassName = "com/floreysoft/jmte/AbstractCompiledTemplate";
+	protected transient String className;
+	protected transient String typeDescriptor;
+	protected transient StringWriter writer;
 
 	public Compiler(String template, Engine engine) {
 		this.template = template;
 		this.engine = engine;
 	}
 
-	public Template compile() {
+	private void initCompilation() {
 		scopes.clear();
-		ClassWriter classWriter = new ClassWriter(0);
-		StringWriter w = new StringWriter();
-		PrintWriter pw = new PrintWriter(w);
-		classVisitor = new TraceClassVisitor(classWriter, pw);
+		className = uniqueNameGenerator.nextUniqueName();
+		typeDescriptor = "L" + className + ";";
+		classWriter = new ClassWriter(0);
+		writer = new StringWriter();
+		classVisitor = new TraceClassVisitor(classWriter, new PrintWriter(
+				writer));
+	}
+
+	public Template compile() {
+		initCompilation();
 		writeHeader();
 
 		List<StartEndPair> scan = engine.scan(template);
@@ -104,7 +133,8 @@ public class Compiler {
 		}
 
 		classVisitor.visitEnd();
-		System.out.println(w.toString());
+		// FIXME: Only for debugging
+		System.out.println(writer.toString());
 		byte[] byteArray = classWriter.toByteArray();
 		Class<?> myClass = Compiler.loadClass(byteArray);
 		try {
@@ -134,9 +164,6 @@ public class Compiler {
 	}
 
 	private void writeHeader() {
-		String superClassName = "com/floreysoft/jmte/AbstractCompiledTemplate";
-		String className = "com/floreysoft/jmte/compiledTemplates/Test";
-		String typeDescriptor = "L" + className + ";";
 
 		MethodVisitor mv;
 
