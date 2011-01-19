0b68420a9d177a6b4cd8bb10373dd51007008106
oliver.zeigermann <oliver.zeigermann@e0cfbd62-f62f-11d8-30af-f3ed57e6e8f3>
2011-01-19 16:53:56 +0000

    - more compiler work - fixed bug: prefix and suffix now only get applied when rendered string is not empty

---
+++ trunk/test/com/floreysoft/jmte/AssortedCompiledTemplateSamples.java
@@ -0,0 +1,36 @@
+package com.floreysoft.jmte;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// ${<h1>,address(NIX),</h1>;long(full)}
+public class AssortedCompiledTemplateSamples extends
+		AbstractCompiledTemplate {
+
+	public AssortedCompiledTemplateSamples(Engine engine) {
+		super(engine);
+	}
+
+	@Override
+	public Set<String> getUsedVariables() {
+		Set<String> usedVariables = new HashSet<String>();
+		usedVariables.add("address");
+		return usedVariables;
+	}
+
+	@Override
+	protected String transformCompiled(ScopedMap model) {
+		StringBuilder buffer = new StringBuilder();
+
+		buffer.append("PREFIX");
+		
+		buffer.append(new StringToken("address", "address", "NIX", "<h1>",
+				"</h1>", "long", "full").evaluate(getEngine(), model,
+				getEngine().getErrorHandler()).toString());
+
+		buffer.append("SUFFIX");
+		
+		return buffer.toString();
+	}
+
+}
