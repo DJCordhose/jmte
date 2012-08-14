908c6735613c0c34471d9ccec4aae4e0b53e7105
daniel.florey@gmail.com <daniel.florey@gmail.com@e0cfbd62-f62f-11d8-30af-f3ed57e6e8f3>
2012-08-14 13:59:27 +0000

    Added clone() and emit() for all tokens

--- trunk/src/com/floreysoft/jmte/token/PlainTextToken.java
+++ trunk/src/com/floreysoft/jmte/token/PlainTextToken.java
@@ -2,15 +2,18 @@ package com.floreysoft.jmte.token;
 
 import com.floreysoft.jmte.TemplateContext;
 
-
 public class PlainTextToken extends AbstractToken {
-	
 	public PlainTextToken(String text) {
 		setText(text);
 	}
 
 	@Override
 	public Object evaluate(TemplateContext context) {
+		return getText();
+	}
+
+	@Override
+	public String emit() {
 		return getText();
 	}
 }
